15b3c2f9693be32b209e997d81a6bebff58b92c3
ArkWavem <adfelix.oficial@gmail.com>
2025-03-15 10:41:54 -0300

    feata: Atividadesjava

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+     //Média de Três números
+        int num01 = 70;
+        int num02 = 20;
+        int num03 = 55;
+        int media = (num01+num02+num03)/3;
+        double media01 = (num01+num02+num03)/3.0;
+        float media02 = (num01+num02+num03)/3.0f;
+        long media03 = (num01+num02+num03)/3L;
+
+        System.out.println("A média do inteiro é: " + media);
+        System.out.println("A média do número real é: " + media01);
+        System.out.println("A média do número float é: " + media02);
+        System.out.println("A média do número long é: " + media03);
+
+
+    }
+}
